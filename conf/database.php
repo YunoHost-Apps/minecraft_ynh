251f61d56d69b898338945abcf78092db9dd3fc7
liberodark <liberodark@gmail.com>
2019-01-07 00:33:55 +0100

    add db for CMS

---
+++ conf/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = [
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'LOGIN1',
+		'password' => 'PASSWORD1',
+		'database' => 'DATABASE1',
+		'encoding' => 'utf8',
+	];
+}
