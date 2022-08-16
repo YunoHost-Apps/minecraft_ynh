66e641d9521aec559ae6b25ca21592d61d065372
ericgaspar <junk.eg@free.fr>
2022-08-16 16:37:22 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,8 @@
 #=================================================
 
 # dependencies used by the app
-java_version=17
 
-pkg_dependencies="openjdk-${java_version}-jdk libasound2 libc6-x32 libc6-i386 libxi6 libxrender1 libxtst6"
+pkg_dependencies="openjdk-17-jdk libasound2 libc6-x32 libc6-i386 libxi6 libxrender1 libxtst6"
 
 #=================================================
 # PERSONAL HELPERS
